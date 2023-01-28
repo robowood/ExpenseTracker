73ca0ff6c2ba958cf64e78c1146026e85f3f436c
robowood <satyamb971@gmail.com>
2023-01-28 16:41:15 +0530

    signUp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Authentication from './Components/Auth/Authentication';
+
+
+function App() {
+  return (
+    <div>
+<Authentication></Authentication>
+    </div>
+  );
+}
+
+export default App;
