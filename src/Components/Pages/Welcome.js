6c932af1e1688eadbe1701965d090751dc008003
robowood <satyamb971@gmail.com>
2023-01-29 13:16:19 +0530

    verifyEmail

--- src/Components/Pages/Welcome.js
+++ src/Components/Pages/Welcome.js
@@ -1,39 +1,68 @@
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
-import classes from './Welcome.module.css'
-const Welcome=()=>{
 
-    const verifyEmailHandler=()=>{
-        
+import React, { Fragment, useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
+import classes from './Welcome.module.css'
+import AuthContext from '../../Store/auth-context'
+
+const Welcome = () => {
+    
+ const authCtx=useContext(AuthContext)
+    
+
+    
+
+    const url='https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBA0u2j_pe10AsypQuTme8e1xEDdseCq4M'
+
+    const verifyEmailHandler=(e)=>{
+        e.preventDefault();
+        fetch(url,{
+            method:'POST',
+            body:JSON.stringify({
+                requestType:"VERIFY_EMAIL",
+                idToken:authCtx.token,
+            }),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then((res)=>{
+            const data=res.json();
+            data.then((resp)=>{
+                console.log(resp);
+            })
+        }).catch((err)=>{
+            console.log('err',err)
+        })
     }
+
+
+
+  return (
+    <Fragment>
+       
+        <div className={classes.main}>
+            <div className={classes.left}>
+            Welcome to expance tracker!!!!
+            </div>
+            <div className={classes.right}>
+                Your profile is incomplete.<Link to='/completeprofile'>Complete now</Link>
+            </div>
+        </div>
+        <div className={classes.buttons}>
+       
+        <button className={classes.logout} >logout</button>
+        <button type='submit' onClick={verifyEmailHandler} className={classes.verifyEmail}>Verify Email</button>
+        </div>
+        <div className={classes.line}></div>
+        
+      <div className={classes.form}>
     
-    return (
-        <Fragment>
-           
-            <div className={classes.main}>
-                <div className={classes.left}>
-                Welcome to expance tracker!!!!
-                </div>
-                <div className={classes.right}>
-                    Your profile is incomplete.<Link to='/completeprofile'>Complete now</Link>
-                </div>
-            </div>
-            <div className={classes.buttons}>
-           
-            <button className={classes.logout} >logout</button>
-            <button type='submit' onClick={verifyEmailHandler} className={classes.verifyEmail}>Verify Email</button>
-            </div>
-            <div className={classes.line}></div>
-            
-          <div className={classes.form}>
-            
-            </div>
-            <div className={classes.table}>
-            </div>
-          
-         
-        </Fragment>
-      )
-    
+        </div>
+        <div className={classes.table}>
+        </div>
+      
+     
+    </Fragment>
+  )
 }
+
 export default Welcome;
